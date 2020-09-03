a68ced1c93f47652fab085b2b4d5961749709f42
Sai Teja <saiteja0512@gmail.com>
2020-09-04 05:06:27 +0530

    Created Header with styling

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./Header.css";
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="header">
+        <h1>SPACE-X 🚀 2020 </h1>
+      </div>
+    );
+  }
+}
+
+export default Header;
